4969a2b1c66b4003f10de9507cae3b0c7d03ec4c
akshansh <akshansh456@gmail.com>
2023-03-29 22:43:37 +0530

    models & forms

--- models/enroll/views.py
+++ models/enroll/views.py
@@ -6,5 +6,12 @@ def studinfo(request):
     stud=Student.objects.get(pk=1)#query set ORM
     return render ( request,'enroll/studetails.html', {'std':stud} )
 def showformdata(request):
-    fm=StudentRegistration()
-    return render(request,'enroll/userregistration.html',{'form':fm})
+    if request.method=="POST":
+        fm=StudentRegistration(request.POST)
+        if fm.is_valid():
+             print('from validated')
+             print('Name:',fm.cleaned_data['name'])  
+             print('Email:',fm.cleaned_data['email'])  
+        else:
+             fm=StudentRegistration()
+    return render(request,'enroll/userregistration.html',{'form':fm}) 
