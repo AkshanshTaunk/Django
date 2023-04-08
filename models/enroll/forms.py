6de7eec05ef809befa9773b8f9d30868949de356
akshansh <akshansh456@gmail.com>
2023-04-08 10:17:09 +0530

    model

--- models/enroll/forms.py
+++ models/enroll/forms.py
@@ -1,4 +1,6 @@
 from django import forms
 class StudentRegistration(forms.Form):
     name = forms.CharField(min_length=5,max_length=15)
+    roll_no = forms.IntegerField()
+    agree = forms.BooleanField()
    
--- models/enroll/views.py
+++ models/enroll/views.py
@@ -11,6 +11,8 @@ def showformdata(request):
         if fm.is_valid():
              print('form validated')
              print('Name:',fm.cleaned_data['name'])
+             print('Roll No:',fm.cleaned_data['roll_no'])
+             print('Agree:',fm.cleaned_data['agree'])
     else:
              fm=StudentRegistration()
     return render(request,'enroll/userregistration.html',{'form':fm}) 
