fd2f41d80c8001ebc7b44a86e18201be010a2b41
akshansh <akshansh456@gmail.com>
2023-06-16 14:46:32 +0530

    pre &  post save,init,delete

--- Signals/project/enroll/signals.py
+++ Signals/project/enroll/signals.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.signals import user_logged_in,user_logged_out,user_login_failed
+from django.db.models.signals import pre_init, pre_save, pre_delete, post_init,post_save,post_delete
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+# from django.core.signals import request_started, request_finished, got_request_exception
 
 
 @receiver(user_logged_in, sender=User)
@@ -36,3 +38,77 @@ def login_failed(sender, credentials, request,**kwargs):
      print(f"kwargs: {kwargs}")
 
 # user_login_failed.connect(login_failed, sender=User)
+
+@receiver(pre_save, sender=User)
+def at_beginning_save(sender, instance, **kwargs):
+     print("-------------")
+     print('pre_save signal....')
+     print("sender: ",sender)
+     print('instance: ',instance)
+     print(f"kwargs: {kwargs}")
+
+# pre_save.connect(at_beginning_save, sender=User)
+
+@receiver(post_save, sender=User)
+def at_ending_save(sender, instance, created, **kwargs):
+   if created:
+     print("-------------")
+     print('post_save signal....')
+     print('New Record')
+     print("sender: ",sender)
+     print('instance: ',instance)
+     print('created: ',created)
+     print(f"kwargs: {kwargs}")
+   else:
+     print("-------------")
+     print('post_save signal....')
+     print('update')
+     print("sender: ",sender)
+     print('instance: ',instance)
+     print('created: ',created)
+     print(f"kwargs: {kwargs}") 
+
+# post_save.connect(at_ending_save, sender=User)
+
+@receiver(pre_delete, sender=User)
+def at_beginning_delete(sender, instance, **kwargs):
+     print("-------------")
+     print('pre_delete signal....')
+     print("sender: ",sender)
+     print('instance: ',instance)
+     print(f"kwargs: {kwargs}")
+
+# pre_delete.connect(at_beginning_delete, sender=User)
+
+@receiver(post_delete, sender=User)
+def at_ending_delete(sender, instance, **kwargs):
+     print("-------------")
+     print('post_delete signal....')
+     print("sender: ",sender)
+     print('instance: ',instance)
+     print(f"kwargs: {kwargs}")
+
+# post_delete.connect(at_ending_delete, sender=User)
+
+
+@receiver(pre_init, sender=User)
+def at_begining_init(sender, *args, **kwargs):
+     print("-------------")
+     print('pre_init signal....')
+     print("sender: ",sender)
+     print(f"args: {args}")
+     print(f"kwargs: {kwargs}")
+
+# pre_init.connect(at_begining_init, sender=User)
+
+@receiver(post_init, sender=User)
+def at_ending_init(sender, *args, **kwargs):
+     print("-------------")
+     print('post_init signal....')
+     print("sender: ",sender)
+     print(f"args: {args}")
+     print(f"kwargs: {kwargs}")
+
+# post_init.connect(at_ending_init, sender=User)
+
+
