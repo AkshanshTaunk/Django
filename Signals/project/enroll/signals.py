b0c38fbb24c8ae00512297ad79e57108fb8827e5
akshansh <akshansh456@gmail.com>
2023-06-16 15:19:58 +0530

    pre&post(flush) migrate , connection created through database

--- Signals/project/enroll/signals.py
+++ Signals/project/enroll/signals.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.signals import user_logged_in,user_logged_out,user_login_failed
-from django.db.models.signals import pre_init, pre_save, pre_delete, post_init,post_save,post_delete
+from django.db.models.signals import pre_init, pre_save, pre_delete, post_init,post_save,post_delete,post_migrate,pre_migrate
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-# from django.core.signals import request_started, request_finished, got_request_exception
+from django.core.signals import request_started, request_finished, got_request_exception
+from django.db.backends.signals import connection_created
 
 
 @receiver(user_logged_in, sender=User)
@@ -111,4 +112,69 @@ def at_ending_init(sender, *args, **kwargs):
 
 # post_init.connect(at_ending_init, sender=User)
 
+@receiver(request_started)
+def at_begining_request(sender, environ, **kwargs):
+    print('-----------------')
+    print('at begining request')
+    print('sender: ',sender)
+    print('environ: ',environ)
+    print(f'kwargs: {kwargs}')
 
+# request_started.connect(at_begining_request)
+
+@receiver(request_finished)
+def at_ending_request(sender, **kwargs):
+    print('-----------------')
+    print('at ending request')
+    print('sender: ',sender)
+    print(f'kwargs: {kwargs}')
+
+# request_finished.connect(at_ending_request)
+
+@receiver(got_request_exception)
+def at_request_exception(sender, request, **kwargs):
+    print('-----------------')
+    print('at exception request')
+    print('sender: ',sender)
+    print('request: ',request)
+    print(f'kwargs: {kwargs}')
+
+# got_request_exception.connect(at_request_exception)
+
+@receiver(pre_migrate)
+def before_install_app(sender, app_config, verbosity, interactive, using, plan, apps,**kwargs):
+    print('-----------------')
+    print('before install app')
+    print('App_config: ',app_config)
+    print('verbosity: ',verbosity)
+    print("interaction: ",interactive)
+    print('using: ',using)
+    print('plan: ',plan)
+    print("plan: ",plan)
+    print(f'kwargs: {kwargs}')
+
+#pre_migrate.connect(before_install_app)
+
+@receiver(post_migrate)
+def at_end_migrate_flush(sender, app_config, verbosity, interactive, using, plan, apps,**kwargs):
+    print('-----------------')
+    print('Migrate flush')
+    print('App_config: ',app_config)
+    print('verbosity: ',verbosity)
+    print("interaction: ",interactive)
+    print('using: ',using)
+    print('plan: ',plan)
+    print("plan: ",plan)
+    print(f'kwargs: {kwargs}')
+
+#post_migrate.connect(at_end_migrate_flush)
+
+@receiver(connection_created)
+def con_db(sender, connection, **kwargs):
+    print('-----------------')
+    print('initial connection to the database.....')
+    print('sender: ',sender)
+    print('connection: ',connection)
+    print(f'kwargs: {kwargs}')
+
+# connection_created.connect(con_db)
