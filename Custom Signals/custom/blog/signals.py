e616cde243f110a4030785b75aba8ae5e33a0aeb
akshansh <akshansh456@gmail.com>
2023-07-21 21:32:08 +0530

    got notification

--- Custom Signals/custom/blog/signals.py
+++ Custom Signals/custom/blog/signals.py
@@ -1,11 +1,11 @@
-from django.dispatch import signals, receiver
+from django.dispatch import Signal, receiver
 
 #creating signals
-notification = signals(providing_args=["request","user"])
+notification = Signal(providing_args=["request","user"])
 
 #receiver function
 @receiver (notification)
 def show_notification(sender, **kwargs):
     print(sender)
-    print(f'{**kwargs}')
+    print(f'{kwargs}')
     print("notification")#just for checking before code
--- Custom Signals/custom/blog/views.py
+++ Custom Signals/custom/blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from blog import signals
 
 # Create your views here.
+def home(request):
+    signals.notification.send(sender=None, request=request, user=['akshansh','taunk'])
+    return HttpResponse("This is home page")
