2fa0658d436fa7c370e50a4b74621f979dc8f4bd
akshansh <akshansh456@gmail.com>
2023-08-19 10:24:07 +0530

    correction

--- Custom MiddleWare Types/Class_Based_Middleware/middleware/middleware/settings.py
+++ Custom MiddleWare Types/Class_Based_Middleware/middleware/middleware/settings.py
@@ -51,7 +51,8 @@ MIDDLEWARE = [
     # 'blog.MWare.Brothermiddleware',
     # 'blog.MWare.Fathermiddleware',
     # 'blog.MWare.Mothermiddleware',
-    'blog.Mware.MyExceptionmiddleware',
+    # 'blog.Mware.MyExceptionmiddleware',
+    'blog.Mware.MyProcessMiddleware',
 ]
 
 ROOT_URLCONF = 'middleware.urls'
