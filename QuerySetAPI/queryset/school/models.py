83c1f4b651ad52f98509a7d87449e3a406fcebbe
akshansh <akshansh456@gmail.com>
2023-08-21 21:49:45 +0530

    Union operation created new table teacher

--- QuerySetAPI/queryset/school/models.py
+++ QuerySetAPI/queryset/school/models.py
@@ -8,3 +8,9 @@ class Student(models.Model):
     marks= models.IntegerField()
     pass_date = models.DateField()
     
+class Teacher(models.Model):
+    name = models.CharField(max_length=50)
+    empnum = models.IntegerField(unique=True, null=False)
+    city = models.CharField(max_length=50)
+    salary= models.IntegerField()
+    join_date = models.DateField()
--- QuerySetAPI/queryset/school/views.py
+++ QuerySetAPI/queryset/school/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
-from .models import Student
+from .models import Student,Teacher
 # Create your views here.
 
 def home(request):
-    student_data = Student.objects.order_by('?')           #randomly
+    # student_data = Student.objects.order_by('?')           #randomly
     # student_data = Student.objects.order_by('-marks')     #desc order
     # student_data = Student.objects.order_by('city')   #asc order
     # student_data = Student.objects.exclude(marks=56)
     # student_data = Student.objects.filter(marks=90)
     # student_data = Student.objects.all()
+    # student_data = Student.objects.order_by('id').reverse()[0:2] 
+    # student_data = Student.objects.using('default')       #using(alias)   DATABASE
+    ################# SECOND TABLE TEACHER ######################
+    qs1 = Student.objects.values_list('id','name', named=True)
+    qs2 = Teacher.objects.values_list('id','name', named=True)
+    student_data = qs2.union(qs1)
     print("Return:",student_data)
     print("SQL Query:",student_data.query)
     return render(request,'school/home.html',{'students':student_data})
