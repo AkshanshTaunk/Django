0b339c94a74b7d0e8b2b98bb34c3a87d5b3262d9
akshansh <akshansh456@gmail.com>
2023-08-21 14:55:04 +0530

    filter queryset

--- QuerySetAPI/queryset/school/views.py
+++ QuerySetAPI/queryset/school/views.py
@@ -3,6 +3,8 @@ from .models import Student
 # Create your views here.
 
 def home(request):
-    student_data = Student.objects.all()
+    student_data = Student.objects.filter(marks=90)
+    # student_data = Student.objects.all()
+    print("Return:",student_data)
     print("SQL Query:",student_data.query)
     return render(request,'school/home.html',{'students':student_data})
