8dbac3687eb6d210f0334e772d01ed54ec8b3b1a
akshansh <akshansh456@gmail.com>
2023-08-21 14:57:31 +0530

    exclude queryset

--- QuerySetAPI/queryset/school/views.py
+++ QuerySetAPI/queryset/school/views.py
@@ -3,7 +3,8 @@ from .models import Student
 # Create your views here.
 
 def home(request):
-    student_data = Student.objects.filter(marks=90)
+    student_data = Student.objects.exclude(marks=56)
+    # student_data = Student.objects.filter(marks=90)
     # student_data = Student.objects.all()
     print("Return:",student_data)
     print("SQL Query:",student_data.query)
