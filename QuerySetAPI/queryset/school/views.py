44c26af4eff61710fe81fb1431b8e3004540befd
akshansh <akshansh456@gmail.com>
2023-08-21 19:01:20 +0530

    order_by queryset API

--- QuerySetAPI/queryset/school/views.py
+++ QuerySetAPI/queryset/school/views.py
@@ -3,7 +3,10 @@ from .models import Student
 # Create your views here.
 
 def home(request):
-    student_data = Student.objects.exclude(marks=56)
+    student_data = Student.objects.order_by('?')           #randomly
+    # student_data = Student.objects.order_by('-marks')     #desc order
+    # student_data = Student.objects.order_by('city')   #asc order
+    # student_data = Student.objects.exclude(marks=56)
     # student_data = Student.objects.filter(marks=90)
     # student_data = Student.objects.all()
     print("Return:",student_data)
