732e83aa6bc32c37f387ed7d8f946dcd9d26dad0
akshansh <akshansh456@gmail.com>
2023-08-22 12:14:14 +0530

    get() queryset

--- QuerySetAPI/queryset/school/views.py
+++ QuerySetAPI/queryset/school/views.py
@@ -3,6 +3,7 @@ from .models import Student,Teacher
 # Create your views here.
 
 def home(request):
+    student_data = Student.objects.get(marks='90')
     # student_data = Student.objects.order_by('?')           #randomly
     # student_data = Student.objects.order_by('-marks')     #desc order
     # student_data = Student.objects.order_by('city')   #asc order
@@ -12,9 +13,9 @@ def home(request):
     # student_data = Student.objects.order_by('id').reverse()[0:2] 
     # student_data = Student.objects.using('default')       #using(alias)   DATABASE
     ################# SECOND TABLE TEACHER ######################
-    qs1 = Student.objects.values_list('id','name', named=True)
-    qs2 = Teacher.objects.values_list('id','name', named=True)
-    student_data = qs2.union(qs1)
-    print("Return:",student_data)
-    print("SQL Query:",student_data.query)
+    # qs1 = Student.objects.values_list('id','name', named=True)
+    # qs2 = Teacher.objects.values_list('id','name', named=True)
+    # student_data = qs2.union(qs1)
+    # print("Return:",student_data)
+    # print("SQL Query:",student_data.query)
     return render(request,'school/home.html',{'students':student_data})
