688fbbe462bfd073250956f69400a50ab124b19a
akshansh <akshansh456@gmail.com>
2023-08-23 22:29:43 +0530

    bulk create & update

--- QuerySetAPI/queryset/school/views.py
+++ QuerySetAPI/queryset/school/views.py
@@ -4,7 +4,8 @@ from .models import Student,Teacher
 
 def home(request):
     # student_data = Student.objects.create(name='yesh',roll=104,city='Surat',marks='99',pass_date='2023-03-07')
-    student_data = Student.objects.filter(id=4).update(name='jay',marks='99')
+    # student_data = Student.objects.filter(id=4).update(name='jay',marks='99')
+    # student_data = Student.objects.update_or_create(id=4,name='jay',defaults={'name':'ROhit'})
     # student_data = Student.objects.latest('pass_date')
     # student_data = Student.objects.order_by('?')           #randomly
     # student_data = Student.objects.order_by('-marks')     #desc order
@@ -20,4 +21,13 @@ def home(request):
     # student_data = qs2.union(qs1)
     # print("Return:",student_data)
     # print("SQL Query:",student_data.query)
+#     objs = [Student(name='ABC',roll='105',city='Srinagar',marks='56',pass_date='2021-08-09'),
+#             Student(name='DEF',roll='106',city='ganganagar',marks='45',pass_date='2021-12-05'),
+#             Student(name='GHI',roll='107',city='ganeshnagar',marks='76',pass_date='2023-11-04')]
+#     student_data = Student.objects.bulk_create(objs)
+    all_student_data = Student.objects.all()
+
+    for std in all_student_data:
+        std.city='singapore'
+    student_data=Student.objects.bulk_update(all_student_data,['city'])
     return render(request,'school/home.html',{'students':student_data})
