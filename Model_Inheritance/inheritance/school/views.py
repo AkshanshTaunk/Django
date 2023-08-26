8a44474c9d43045a54e16def7ddf1751aebe34d1
akshansh <akshansh456@gmail.com>
2023-08-26 23:01:44 +0530

    correction

--- Model_Inheritance/inheritance/school/views.py
+++ Model_Inheritance/inheritance/school/views.py
@@ -6,4 +6,4 @@ def home(request):
     student_data=Student.objects.all()
     teacher_data=Teacher.objects.all()
     contractor_data=Contractor.objects.all()
-    return render(request,'school\home.html',{'student':student_data,'teacher':teacher_data,'contractor':contractor_data})
+    return render(request,'school/home.html',{'student':student_data,'teacher':teacher_data,'contractor':contractor_data})
