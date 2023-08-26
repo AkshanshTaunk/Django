3e92e655b15e18253415e9107525646c00e4247d
akshansh <akshansh456@gmail.com>
2023-08-26 17:28:25 +0530

    html file

--- Model_Inheritance/inheritance/school/views.py
+++ Model_Inheritance/inheritance/school/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 from .models import Student,Teacher, Contractor
 # Create your views here.
 
+def home(request):
+    student_data=Student.objects.all()
+    teacher_data=Teacher.objects.all()
+    contractor_data=Contractor.objects.all()
+    return render(request,'school\home.html',{'student':student_data,'teacher':teacher_data,'contractor':contractor_data})
