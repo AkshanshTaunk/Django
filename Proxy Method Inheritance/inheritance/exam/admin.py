3d22c766c08488bf2b40542121a178925d363dde
akshansh <akshansh456@gmail.com>
2023-08-31 22:45:21 +0530

    proxy

--- Proxy Method Inheritance/inheritance/exam/admin.py
+++ Proxy Method Inheritance/inheritance/exam/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ExamCenter,Student
 
 # Register your models here.
+@admin.register(ExamCenter)
+class ExamCenteradmin(admin.ModelAdmin):
+    list_display=['id','cname','city']
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=['id','cname','city']
--- Proxy Method Inheritance/inheritance/exam/models.py
+++ Proxy Method Inheritance/inheritance/exam/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class ExamCenter(models.Model):
+    cname=models.CharField(max_length=30)
+    city=models.CharField(max_length=30)
+
+class Student(ExamCenter):
+    class Meta:
+        proxy = True
