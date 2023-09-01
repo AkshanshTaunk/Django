d8a407599dec865aae95352337cfaa31a326378b
akshansh <akshansh456@gmail.com>
2023-09-01 21:30:39 +0530

    object manager rename to student

--- Custom Manager/manager/manager/settings.py
+++ Custom Manager/manager/manager/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'student',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'D:\Django\Custom Manager\manager\student\template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Custom Manager/manager/student/views.py
+++ Custom Manager/manager/student/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Student
 # Create your views here.
+
+def home(request):
+    student_data=Student.students.all()
+    return render(request,'student/home.html',{'student':student_data})
