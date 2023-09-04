375d81d447aaff057f698d39724962bcd39e1f73
akshansh <akshansh456@gmail.com>
2023-09-04 15:33:06 +0530

    ManytoOne work by using foreign key

--- Model Relationship/Many to One/manytoone/student/models.py
+++ Model Relationship/Many to One/manytoone/student/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Post(models.Model):
+    user1=models.ForeignKey(User,on_delete=models.CASCADE)
+    post_title=models.CharField(max_length=30)
+    post_cat=models.CharField(max_length=30)
+    post_publish_date=models.DateField()
