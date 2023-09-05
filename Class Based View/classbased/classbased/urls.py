f09778c8c6807ad6f21dfce677874e5388cd0e56
akshansh <akshansh456@gmail.com>
2023-09-05 22:24:33 +0530

    classed based as_view()

--- Class Based View/classbased/classbased/urls.py
+++ Class Based View/classbased/classbased/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from views import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('cl/',views.MyView.as_view()),
+    path('subcl/',views.MyViewChild.as_view()),
 ]
--- Class Based View/classbased/views/views.py
+++ Class Based View/classbased/views/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views import View
 # Create your views here.
+
+class MyView(View):
+    name='Akshansh'
+    def get(self,request):
+        return HttpResponse(self.name)
+
+class MyViewChild(MyView):
+    def get(self):
+        return HttpResponse(self.name)
