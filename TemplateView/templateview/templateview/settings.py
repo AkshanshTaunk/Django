8dbd82fa564e021ac04d177853d8c322acd3f77e
akshansh <akshansh456@gmail.com>
2023-09-07 15:44:28 +0530

    templateView running

--- TemplateView/templateview/templateview/settings.py
+++ TemplateView/templateview/templateview/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'templateview.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'D:\Django\TemplateView\templateview\myapp\template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
