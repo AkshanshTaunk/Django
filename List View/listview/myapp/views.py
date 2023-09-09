03082a9c27a6f6cb1337c48c24601f17cbe133ef
akshansh <akshansh456@gmail.com>
2023-09-09 14:55:01 +0530

    create specific format template for Listview stduent_list.html

--- List View/listview/myapp/views.py
+++ List View/listview/myapp/views.py
@@ -1,3 +1,13 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from .models import Student
 # Create your views here.
+
+class StudentListView(ListView):
+    model = Student
+    # ordering = ['name']
+    
+    def get_queryset(self):
+        return Student.objects.filter(course='C++')
