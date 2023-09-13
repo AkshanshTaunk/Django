53d396d33037d4a0f86f569daf8335fa8da1ff42
akshansh <akshansh456@gmail.com>
2023-09-13 15:02:39 +0530

    ListView complted with get_context_data

--- List View/listview/myapp/views.py
+++ List View/listview/myapp/views.py
@@ -11,3 +11,9 @@ class StudentListView(ListView):
     
     def get_queryset(self):
         return Student.objects.filter(course='C++')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["freshers"] = Student.objects.filter(roll=101)
+        return context
+    
