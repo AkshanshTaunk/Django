cb6297494c90086b4e099423d6c40bd27357c3aa
akshansh <akshansh456@gmail.com>
2023-09-19 23:11:35 +0530

    5

--- Create View/createview/myapp/views.py
+++ Create View/createview/myapp/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import Student
+from django import forms
 # Create your views here.
 
 class StudentCreateView(CreateView):
     model = Student
     fields = ['name','email','password']
     success_url = '/create/'
+
+    def get_form(self):
+        form = super().get_form()
+        form.fields['name'].widget=forms.TextInput(attrs={'class':'myclass'})
+        form.fields['password'].widget=forms.PasswordInput(attrs={'class':'mypass'})
+        return form
