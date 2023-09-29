184f5d6a8372dccb72668b855da3b7d08b6597d8
akshansh <akshansh456@gmail.com>
2023-09-29 14:03:38 +0530

    forms.py implemented

---
+++ Authentication View/FunctionBased/auth/myapp/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm,UsernameField
+from django.utils.translation import gettext,gettext_lazy as _
+class LoginForm(AuthenticationForm):
+    username = UsernameField(widget=forms.TextInput(attrs={'autofocus':True,'class':'myuser'}))
+    password = forms.CharField(label=_("Password"),
+                               strip=False,
+                               widget=forms.TextInput(attrs={'autocomplete':'current-password','class':'mypass'}),
+                               )
