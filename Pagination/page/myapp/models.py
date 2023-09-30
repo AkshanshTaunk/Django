baddbc14396982da879ef0cd909113cfc8818e37
akshansh <akshansh456@gmail.com>
2023-09-30 23:06:40 +0530

    3

--- Pagination/page/myapp/models.py
+++ Pagination/page/myapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=90)
+    desc = models.TextField(max_length=900)
+    pub_date = models.DateTimeField()
